64758ab95c53ae7c134165a05c8987d3078cb93b
woong <koohw0109@gmail.com>
2023-08-06 11:57:24 +0900

    multiblocprovider

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -5,6 +5,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ulearning_app/pages/welcome/bloc/welcome_blocs.dart';
 import 'package:ulearning_app/pages/welcome/bloc/welcome_events.dart';
 
+import '../../main.dart';
 import 'bloc/welcome_states.dart';
 
 class Welcome extends StatefulWidget {
@@ -15,6 +16,8 @@ class Welcome extends StatefulWidget {
 }
 
 class _WelcomeState extends State<Welcome> {
+  PageController pageController = PageController(initialPage: 0);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -29,6 +32,7 @@ class _WelcomeState extends State<Welcome> {
                 alignment: Alignment.topCenter,
                 children: [
                   PageView(
+                    controller: pageController,
                     onPageChanged: (index) {
                       state.page = index;
                       BlocProvider.of<WelcomeBloc>(context).add(
@@ -42,7 +46,7 @@ class _WelcomeState extends State<Welcome> {
                         "next",
                         "First See Learning",
                         "Forget about a for of paper all knowledge in one learning!",
-                        "image path",
+                        "assets/images/reading.png",
                       ),
                       _page(
                         2,
@@ -50,7 +54,7 @@ class _WelcomeState extends State<Welcome> {
                         "next",
                         "Connect With Everyone",
                         "Always keep in touch with your tutor & friend. let's get connected!",
-                        "image path",
+                        "assets/images/boy.png",
                       ),
                       _page(
                         3,
@@ -58,7 +62,7 @@ class _WelcomeState extends State<Welcome> {
                         "get started",
                         "Always Fascinated Learning",
                         "Anywhere, anytime. The tume is your discretion so study whenever you want.",
-                        "image path",
+                        "assets/images/man.png",
                       ),
                     ],
                   ),
@@ -101,8 +105,9 @@ class _WelcomeState extends State<Welcome> {
         SizedBox(
           width: 345.w,
           height: 345.w,
-          child: Text(
+          child: Image.asset(
             imagePath,
+            fit: BoxFit.cover,
           ),
         ),
         Container(
@@ -130,35 +135,53 @@ class _WelcomeState extends State<Welcome> {
             ),
           ),
         ),
-        Container(
-          margin: EdgeInsets.only(
-            top: 100.h,
-            left: 25.w,
-            right: 25.w,
-          ),
-          width: 325.w,
-          height: 50.h,
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.all(
-              Radius.circular(15.w),
+        GestureDetector(
+          onTap: () {
+            // within 0-2 index
+            if (index < 3) {
+              // animation
+              pageController.animateToPage(
+                index,
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.easeIn,
+              );
+            } else {
+              // jump to a next page
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const MyHomePage(),
+              ));
+            }
+          },
+          child: Container(
+            margin: EdgeInsets.only(
+              top: 100.h,
+              left: 25.w,
+              right: 25.w,
             ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.1),
-                spreadRadius: 1,
-                blurRadius: 2,
-                offset: const Offset(0, 1),
+            width: 325.w,
+            height: 50.h,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.all(
+                Radius.circular(15.w),
               ),
-            ],
-          ),
-          child: Center(
-            child: Text(
-              buttonName,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16.sp,
-                fontWeight: FontWeight.normal,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.1),
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                  offset: const Offset(0, 1),
+                ),
+              ],
+            ),
+            child: Center(
+              child: Text(
+                buttonName,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.normal,
+                ),
               ),
             ),
           ),
