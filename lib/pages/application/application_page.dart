7d7d392966f028b649a16fd13fe98ff6f763029f
woong <koohw0109@gmail.com>
2023-08-07 16:14:45 +0900

    icons & elevation btmnavbar

--- lib/pages/application/application_page.dart
+++ lib/pages/application/application_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ulearning_app/common/values/colors.dart';
 import 'package:ulearning_app/pages/application/widgets/application_widgets.dart';
 
 class ApplicationPage extends StatefulWidget {
@@ -19,32 +20,119 @@ class _ApplicationPageState extends State<ApplicationPage> {
       child: SafeArea(
         child: Scaffold(
           body: buildPage(_index),
-          bottomNavigationBar: BottomNavigationBar(
-            onTap: (value) {
-              setState(() {
-                _index = value;
-              });
-              print(_index);
-            },
-            elevation: 0,
-            items: [
-              BottomNavigationBarItem(
-                label: "Home",
-                icon: SizedBox(
-                  width: 15.w,
-                  height: 15.h,
-                  child: Image.asset("assets/icons/home.png"),
-                ),
+          bottomNavigationBar: Container(
+            width: 375.w,
+            height: 58.h,
+            decoration: BoxDecoration(
+              color: AppColors.primaryElement,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20.h),
+                topRight: Radius.circular(20.h),
               ),
-              BottomNavigationBarItem(
-                label: "Search",
-                icon: SizedBox(
-                  width: 15.w,
-                  height: 15.h,
-                  child: Image.asset("assets/icons/home.png"),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.1),
+                  spreadRadius: 1,
+                  blurRadius: 1,
                 ),
-              ),
-            ],
+              ],
+            ),
+            child: BottomNavigationBar(
+              currentIndex: _index,
+              onTap: (value) {
+                setState(() {
+                  _index = value;
+                });
+              },
+              elevation: 0,
+              type: BottomNavigationBarType.fixed,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              selectedItemColor: AppColors.primaryElement,
+              unselectedItemColor: AppColors.primaryFourthElementText,
+              items: [
+                BottomNavigationBarItem(
+                  label: "home",
+                  icon: SizedBox(
+                    width: 15.w,
+                    height: 15.h,
+                    child: Image.asset("assets/icons/home.png"),
+                  ),
+                  activeIcon: SizedBox(
+                    width: 15.w,
+                    height: 15.h,
+                    child: Image.asset(
+                      "assets/icons/home.png",
+                      color: AppColors.primaryElement,
+                    ),
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  label: "search",
+                  icon: SizedBox(
+                    width: 15.w,
+                    height: 15.h,
+                    child: Image.asset("assets/icons/search2.png"),
+                  ),
+                  activeIcon: SizedBox(
+                    width: 15.w,
+                    height: 15.h,
+                    child: Image.asset(
+                      "assets/icons/search2.png",
+                      color: AppColors.primaryElement,
+                    ),
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  label: "course",
+                  icon: SizedBox(
+                    width: 15.w,
+                    height: 15.h,
+                    child: Image.asset("assets/icons/play-circle1.png"),
+                  ),
+                  activeIcon: SizedBox(
+                    width: 15.w,
+                    height: 15.h,
+                    child: Image.asset(
+                      "assets/icons/play-circle1.png",
+                      color: AppColors.primaryElement,
+                    ),
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  label: "chat",
+                  icon: SizedBox(
+                    width: 15.w,
+                    height: 15.h,
+                    child: Image.asset("assets/icons/message-circle.png"),
+                  ),
+                  activeIcon: SizedBox(
+                    width: 15.w,
+                    height: 15.h,
+                    child: Image.asset(
+                      "assets/icons/message-circle.png",
+                      color: AppColors.primaryElement,
+                    ),
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  label: "profile",
+                  icon: SizedBox(
+                    width: 15.w,
+                    height: 15.h,
+                    child: Image.asset("assets/icons/person2.png"),
+                  ),
+                  activeIcon: SizedBox(
+                    width: 15.w,
+                    height: 15.h,
+                    child: Image.asset(
+                      "assets/icons/person2.png",
+                      color: AppColors.primaryElement,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
