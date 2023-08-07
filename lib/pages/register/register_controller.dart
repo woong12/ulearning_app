ee831b1b38db7a4010a48303e7ce97523ddc50a2
woong <koohw0109@gmail.com>
2023-08-07 15:20:07 +0900

    register controller

---
+++ lib/pages/register/register_controller.dart
@@ -0,0 +1,64 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ulearning_app/common/widgets/flutter_toast.dart';
+import 'package:ulearning_app/pages/register/bloc/register_blocs.dart';
+
+class RegisterController {
+  final BuildContext context;
+  const RegisterController(this.context);
+
+  void handleEmailRegister() async {
+    final state = context.read<RegisterBlocs>().state;
+    String userName = state.userName;
+    String email = state.email;
+    String password = state.password;
+    String rePassword = state.rePassword;
+
+    if (userName.isEmpty) {
+      toastInfo(msg: "User name can not be empty");
+      return;
+    }
+
+    if (email.isEmpty) {
+      toastInfo(msg: "Email can not be empty");
+      return;
+    }
+
+    if (password.isEmpty) {
+      toastInfo(msg: "Password can not be empty");
+      return;
+    }
+
+    if (rePassword.isEmpty) {
+      toastInfo(msg: "Your password confirmation is wrond");
+      return;
+    }
+
+    try {
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      if (credential.user != null) {
+        await credential.user?.sendEmailVerification();
+        await credential.user?.updateDisplayName(userName);
+        toastInfo(
+          msg:
+              "An email has been sent to your registered email. To activate it please check your email box and click on the link",
+        );
+        Navigator.of(context).pop();
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        toastInfo(msg: "The password provided is too weak");
+      } else if (e.code == 'email-already-in-use') {
+        toastInfo(msg: "The email is already in use");
+      } else if (e.code == 'invalid-email') {
+        toastInfo(msg: "Your email id is invalid");
+      }
+    }
+  }
+}
