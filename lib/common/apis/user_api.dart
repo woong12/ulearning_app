99d9ea9c5e4af094cd80411d650be1367d17690b
woong <koohw0109@gmail.com>
2023-08-11 10:15:26 +0900

    account

--- lib/common/apis/user_api.dart
+++ lib/common/apis/user_api.dart
@@ -1,14 +1,14 @@
-import 'package:ulearning_app/common/entities/user.dart';
+// import 'package:ulearning_app/common/entities/user.dart';
 
-import '../utils/http_util.dart';
+// import '../utils/http_util.dart';
 
-class UserAPI {
-  static login(
-      {LoginRequestEntity? params, required LoginRequestEntity param}) async {
-    var response = await HttpUtil().post(
-      'api/login',
-      queryParameters: params?.toJson(),
-    );
-    print(response.toString());
-  }
-}
+// class UserAPI {
+//   static login(
+//       {LoginRequestEntity? params, required LoginRequestEntity param}) async {
+//     var response = await HttpUtil().post(
+//       'api/login',
+//       queryParameters: params?.toJson(),
+//     );
+//     print(response.toString());
+//   }
+// }
